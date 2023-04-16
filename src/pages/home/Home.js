64584ed26e028f3090c98231a32d4f44e835a555
puneet0408 <punitvirmani725@gmail.com>
2023-04-16 11:27:15 +0530

    Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./style.scss"
+import "./Style.scss"
 import HeroBanner from "./heroBanner/HeroBanner"
 import Trending from './trending/Trending'
 import Popular from './popular/Popular'
--- src/pages/searchResult/SearchResult.js
+++ src/pages/searchResult/SearchResult.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
-import "./style.scss";
+import "./Style.scss";
 import { fetchDataFromApi } from "../../Utils/Api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import Spinner from "../../components/spinner/Spinner";
