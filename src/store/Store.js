b531721c5b0a89f238896999bb9a51a55929921e
puneet0408 <punitvirmani725@gmail.com>
2023-04-16 11:18:57 +0530

    header

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,11 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import homeSlice from "./homeSlice";
-//import { combineReducers } from "@reduxjs/toolkit";
-
-
-// const reducer = combineReducers({
-//   home: homeSlice,
-// })
+import homeSlice from "./HomeSlice";
 export const store = configureStore({
     reducer: {
         home: homeSlice
