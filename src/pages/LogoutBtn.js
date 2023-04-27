bf00c9dce065dd39ec3323301af70f8ab5ef49ad
puneet0408 <punitvirmani725@gmail.com>
2023-04-27 18:48:50 +0530

    Login

--- src/pages/LogoutBtn.js
+++ src/pages/LogoutBtn.js
@@ -3,18 +3,19 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { auth } from "../firebase/Firebase"
 import { Logout } from "../store/UserAuth"
+
 function LogoutBtn() {
   const dispatch = useDispatch();
-
   const authSignOut = () => {
     signOut(auth).then(() => {
       localStorage.clear();
       clear();
       dispatch(Logout())
     }).catch((error) => {
-      // An error happened.
+  
     });
   }
+ 
   return (
     <div className='loginForm' >
       <button onClick={authSignOut} style={{
@@ -24,7 +25,7 @@ function LogoutBtn() {
         height: "100%",
         color: "#fff",
         fontSize: "20px",
-        textAlign:"start"
+        textAlign: "start"
       }} >Logout</button>
     </div>
   )
